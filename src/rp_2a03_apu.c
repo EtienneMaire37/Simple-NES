8c95fa074a511b5d256b3ce1334d8d8db115bcc4
EtienneMaire <layzy37@gmail.com>
2024-12-12 17:53:20 +0100

    Added square wave support

--- src/rp_2a03_apu.c
+++ src/rp_2a03_apu.c
@@ -1,30 +1,31 @@
 #pragma once
 
-void apu_reset(APU *apu)
+void apu_reset(APU* apu)
 {
-    ;
+    apu->pulse1_duty_cycle = 0.5;
+    apu->pulse1_frequency = 440;
 }
 
-void apu_init(APU *apu) 
+void apu_init(APU* apu) 
 {
     apu->wave_out = 0;
-    apu->time = 0;
+    apu->pulse1_time = 0;
     apu->current_buffer = 0;
 
     WAVEFORMATEX wfx = { WAVE_FORMAT_PCM, 1, APU_SAMPLE_RATE, APU_SAMPLE_RATE, 1, 8, 0 };
     apu->wfx = wfx;
 
-    if (waveOutOpen(&apu->wave_out, WAVE_MAPPER, &apu->wfx, (DWORD_PTR)wave_out_callback, (DWORD_PTR)apu, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) 
+    if (waveOutOpen(&apu->wave_out, WAVE_MAPPER, &apu->wfx, (DWORD_PTR)apu_wave_out_callback, (DWORD_PTR)apu, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) 
     {
-        printf("Failed to open audio device.\n");
+        printf("Failed to open audio device\n");
         return;
     }
 
     for (int i = 0; i < APU_NUM_BUFFERS; i++) 
     {
-        fill_buffer(apu, apu->buffers[i], APU_BUFFER_SIZE);
+        apu_fill_buffer(apu, apu->buffers[i], APU_BUFFER_SIZE);
 
-        WAVEHDR *hdr = &apu->wave_headers[i];
+        WAVEHDR* hdr = &apu->wave_headers[i];
         hdr->lpData = (LPSTR)apu->buffers[i];
         hdr->dwBufferLength = APU_BUFFER_SIZE;
         hdr->dwFlags = 0;
@@ -32,49 +33,81 @@ void apu_init(APU *apu)
 
         if (waveOutPrepareHeader(apu->wave_out, hdr, sizeof(WAVEHDR)) != MMSYSERR_NOERROR) 
         {
-            printf("Failed to prepare audio buffer %d.\n", i);
+            printf("Failed to prepare audio buffer %d\n", i);
             return;
         }
 
         if (waveOutWrite(apu->wave_out, hdr, sizeof(WAVEHDR)) != MMSYSERR_NOERROR) 
         {
-            printf("Failed to write audio buffer %d.\n", i);
+            printf("Failed to write audio buffer %d\n", i);
             return;
         }
     }
 }
 
-static void fill_buffer(APU *apu, uint8_t *buffer, size_t size) 
+float apu_getchannel(APU* apu, uint8_t channel)
 {
-    for (size_t i = 0; i < size; i++) {
-        float val = sinf(apu->time) * 0.5f + 0.5f;
-        apu->time += 2.0f * M_PI * 440.0f / (float)APU_SAMPLE_RATE;
-        buffer[i] = (uint8_t)(val * 255);
+    if (!(channel >= 0 && channel < 5))
+        return 0;
+
+    switch (channel)
+    {
+    case APU_CHANNEL_PULSE1:
+    {
+        float amplitude = 0.5;
+        float omega = 2 * M_PI * apu->pulse1_frequency;
+        float val = 0;
+        apu->pulse1_time += omega / (float)APU_SAMPLE_RATE;
+        for (uint8_t i = 1; i <= APU_PULSE_WAVE_HARMONICS; i++)
+            val += sin(M_PI * i * apu->pulse1_duty_cycle) * cos(i * apu->pulse1_time) / i;
+        val *= 2 * amplitude / M_PI;
+        val += amplitude * apu->pulse1_duty_cycle + 0.125;
+        return val * 15;
+    }
+
+    default:
+        return 0;
     }
 }
 
-static void CALLBACK wave_out_callback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) 
+float apu_pulse_out(APU* apu)
+{
+    float sum = apu_getchannel(apu, 0) + apu_getchannel(apu, 1);
+    if (sum == 0)
+        return 0;
+    float out = 95.88f / ((8128 / sum) + 100);
+    return max(0, min(1, out));
+}
+
+static void apu_fill_buffer(APU* apu, uint8_t* buffer, size_t size) 
+{
+    for (size_t i = 0; i < size; i++) 
+    {
+        float val = apu_pulse_out(apu);
+        buffer[i] = 0; //(uint8_t)(val * APU_VOLUME * 255);
+    }
+}
+
+static void CALLBACK apu_wave_out_callback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2) 
 {
     if (uMsg == WOM_DONE) 
     {
-        APU *apu = (APU *)dwInstance;
+        APU* apu = (APU*)dwInstance;
 
         WAVEHDR *hdr = &apu->wave_headers[apu->current_buffer];
-        fill_buffer(apu, (uint8_t *)hdr->lpData, hdr->dwBufferLength);
+        apu_fill_buffer(apu, (uint8_t*)hdr->lpData, hdr->dwBufferLength);
 
         if (waveOutWrite(apu->wave_out, hdr, sizeof(WAVEHDR)) != MMSYSERR_NOERROR) 
         {
-            fprintf(stderr, "Failed to write audio buffer.\n");
+            printf("Failed to write audio buffer\n");
         }
 
         apu->current_buffer = (apu->current_buffer + 1) % APU_NUM_BUFFERS;
     }
 }
 
-void apu_destroy(APU *apu) 
+void apu_destroy(APU* apu) 
 {
-    waveOutReset(apu->wave_out);
-
     for (int i = 0; i < APU_NUM_BUFFERS; i++) 
         waveOutUnprepareHeader(apu->wave_out, &apu->wave_headers[i], sizeof(WAVEHDR));
 
--- src/rp_2a03_apu.h
+++ src/rp_2a03_apu.h
@@ -4,15 +4,29 @@
 #define APU_BUFFER_SIZE (APU_SAMPLE_RATE / 30)
 #define APU_NUM_BUFFERS 4
 
+#define APU_PULSE_WAVE_HARMONICS        16
+#define APU_TRIANGLE_WAVE_HARMONICS     6
+
+#define APU_CHANNEL_PULSE1      0
+#define APU_CHANNEL_PULSE2      1
+#define APU_CHANNEL_TRIANGLE    2
+#define APU_CHANNEL_NOISE       3
+#define APU_CHANNEL_DMC         4
+
+#define APU_VOLUME              0.5
+
 typedef struct RP_2A03_APU
 {
     HWAVEOUT wave_out;
     WAVEFORMATEX wfx;
     WAVEHDR wave_headers[APU_NUM_BUFFERS];
     uint8_t buffers[APU_NUM_BUFFERS][APU_BUFFER_SIZE];
-    float time;
     int current_buffer;
+
+    float pulse1_time;
+    float pulse1_duty_cycle;
+    float pulse1_frequency;
 } APU;
 
-static void CALLBACK wave_out_callback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
-static void fill_buffer(APU *apu, uint8_t *buffer, size_t size);
+static void CALLBACK apu_wave_out_callback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
+static void apu_fill_buffer(APU *apu, uint8_t *buffer, size_t size);
